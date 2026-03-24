d755810b23ca33d71ccf16ba5ea731c798f8181b
clemaxil <clemaxil25@gmail.com>
2026-03-24 09:15:54 +0100

    update v0.03

--- extras/ipset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-if ! command -v ipset >/dev/null; then
-	echo "ERROR : You need to install package ipset"
-	exit 1
-fi
