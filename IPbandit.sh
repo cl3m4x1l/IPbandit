546b34c9c7e14707fcedd81ef74a9d74b2c1018b
cl3m4x1l <clemaxil25@gmail.com>
2026-03-25 07:20:21 +0100

    Version Alpha

--- IPbandit.sh
+++ IPbandit.sh
@@ -45,11 +45,11 @@ while IFS= read -r url; do
 
     # Téléchargement
     if curl -fsSL --retry 3 "$url" -o "$tmpfile"; then
-        
+
         # Détection gzip via la commande file
         if file "$tmpfile" | grep -qi 'gzip'; then
             echo "gzip compressed detected, décompress..."
-            
+
             if gunzip -c "$tmpfile" > "${i}.list"; then
                 echo "Save (ungzip) in ${i}.list"
                 ((i++))
